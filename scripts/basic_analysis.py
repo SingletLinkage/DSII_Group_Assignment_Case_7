1e73de174c8ffc35684d157004d2ccb2f8ca79cd
Pranab <pranabray232005@gmail.com>
2024-03-26 11:24:46 +0530

    types of products

---
+++ scripts/basic_analysis.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+data=pd.read_csv('source_files\\random_database.csv')
+plt.figure(figsize=(10, 6))
+plt.hist(data['Price'], bins=30, color='skyblue', edgecolor='black')
+plt.title('Histogram of Lip Product Prices')
+plt.xlabel('Price')
+plt.xticks(rotation=90)
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
