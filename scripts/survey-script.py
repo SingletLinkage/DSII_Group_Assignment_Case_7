4f3ca3c0b6e61c9c3d1f0c3c1ee8529dfe660b89
Arka <arkam27112004@gmail.com>
2024-04-11 21:17:13 +0530

    Final Changes

--- scripts/survey-script.py
+++ scripts/survey-script.py
@@ -24,41 +24,41 @@ data_others = data[data['Gender']=='Prefer not to say']
 # Plotting the data:
 
 # Gender Distribution:
-# x = data['Gender'].value_counts()
-# label = data['Gender'].value_counts().index
-# plt.pie(x, labels=label, autopct='%1.1f%%')
-# center_circle = plt.Circle((0, 0), 0.7, color='white')
-# plt.gca().add_artist(center_circle)
-# plt.title('Gender Distribution in Responses', fontsize=15)
-# plt.savefig('images/survey-graphs/gender-distribution.pdf')
+x = data['Gender'].value_counts()
+label = data['Gender'].value_counts().index
+plt.pie(x, labels=label, autopct='%1.1f%%')
+center_circle = plt.Circle((0, 0), 0.7, color='white')
+plt.gca().add_artist(center_circle)
+plt.title('Gender Distribution in Responses', fontsize=15)
+plt.savefig('images/survey-graphs/gender-distribution.pdf')
 
 # Individual Brand Preferences:
 # plot all brands as a bar plot
 
-brands = ['Nykaa', 'Revlon', 'Mamaearth', 'Lotus Herbals', 'Elle 18', 'Maybelline', 'Lakme', 'Sugar Cosmetics', 'Miss Claire', 'L\'Oreal Paris']
-params = {
-    'not_heard': 0, 
-    'not_used':  0, 
-    'rating_1':  0, 
-    'rating_2':  0, 
-    'rating_3':  0
-}
+# brands = ['Nykaa', 'Revlon', 'Mamaearth', 'Lotus Herbals', 'Elle 18', 'Maybelline', 'Lakme', 'Sugar Cosmetics', 'Miss Claire', 'L\'Oreal Paris']
+# params = {
+#     'not_heard': 0, 
+#     'not_used':  0, 
+#     'rating_1':  0, 
+#     'rating_2':  0, 
+#     'rating_3':  0
+# }
 
-all_details = {}
+# all_details = {}
 
-for brand in brands:
-    all_details[brand] = params.copy()
-    all_details[brand]['not_heard'] = data[brand].value_counts()['Haven\'t heard about this']
-    all_details[brand]['not_used'] = data[brand].value_counts()['Heard about it - havent used it']
-    all_details[brand]['rating_1'] = data[brand].value_counts()['Used it, Rating - 1/3']
-    all_details[brand]['rating_2'] = data[brand].value_counts()['Used it, Rating - 2/3']
-    all_details[brand]['rating_3'] = data[brand].value_counts()['Used it, Rating - 3/3']
+# for brand in brands:
+#     all_details[brand] = params.copy()
+#     all_details[brand]['not_heard'] = data[brand].value_counts()['Haven\'t heard about this']
+#     all_details[brand]['not_used'] = data[brand].value_counts()['Heard about it - havent used it']
+#     all_details[brand]['rating_1'] = data[brand].value_counts()['Used it, Rating - 1/3']
+#     all_details[brand]['rating_2'] = data[brand].value_counts()['Used it, Rating - 2/3']
+#     all_details[brand]['rating_3'] = data[brand].value_counts()['Used it, Rating - 3/3']
 
-# print(all_details)
+# # print(all_details)
 
-brands = ['Nykaa', 'Revlon', 'Mamaearth', 'Lotus Herbals', 'Elle 18', 'Maybelline', 'Lakme', 'Sugar Cosmetics', 'Miss Claire', 'L\'Oreal Paris']
-for brand in brands:
-    plot_donut(all_details[brand].values(), labels=['Haven\'t Heard', 'Haven\'t Used', 'Rating: 1', 'Rating: 2', 'Rating: 3'], title=f'{brand} Brand Preference', filename=f'images/survey-graphs/{brand}-brand-preference.pdf')
+# brands = ['Nykaa', 'Revlon', 'Mamaearth', 'Lotus Herbals', 'Elle 18', 'Maybelline', 'Lakme', 'Sugar Cosmetics', 'Miss Claire', 'L\'Oreal Paris']
+# for brand in brands:
+#     plot_donut(all_details[brand].values(), labels=['Haven\'t Heard', 'Haven\'t Used', 'Rating: 1', 'Rating: 2', 'Rating: 3'], title=f'{brand} Brand Preference', filename=f'images/survey-graphs/{brand}-brand-preference.pdf')
 
 
-plt.show()
+# plt.show()
